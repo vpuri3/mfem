74f163e2a0d50b0e23077d26f109a2831c32e044
psocratis <petrides1@llnl.gov>
2022-11-28 17:39:50 -0800

    style

--- miniapps/dpg/pmaxwell-tokamak.cpp
+++ miniapps/dpg/pmaxwell-tokamak.cpp
@@ -2,14 +2,14 @@
 // srun -n 256 ./pmaxwell-tokamak -o 3 -sc -rnum
 // srun -n 448 ./pmaxwell-tokamak -o 4 -sc -rnum 11.0 -sigma 2.0 -paraview
 
-// Description:  
+// Description:
 // This example code demonstrates the use of MFEM to define and solve
 // the "ultraweak" (UW) DPG formulation for the Maxwell problem
 
 //      ∇×(1/μ ∇×E) - (ω^2 ϵ + i ω σ) E = Ĵ ,   in Ω
 //                E×n = E_0, on ∂Ω
 
-// It solves the following kinds of problems 
+// It solves the following kinds of problems
 // 1) Known exact solutions with error convergence rates
 //    a) A manufactured solution problem where E is a plane beam
 // 2) Fichera "microwave" problem
@@ -18,28 +18,28 @@
 //    b) Plane wave scattering from a square
 //    c) PML problem with a point source prescribed on the boundary
 
-// 
+//
 // The DPG UW deals with the First Order System
 //        i ω μ H + ∇ × E = 0,   in Ω (Faraday's law)
 // -(i ω ϵ + σ) E + ∇ × H = J,   in Ω (Ampere's law)
 //            E × n = E_0, on ∂Ω
 // Note: Ĵ = -iωJ
 
-// The ultraweak-DPG formulation is obtained by integration by parts of both 
+// The ultraweak-DPG formulation is obtained by integration by parts of both
 // equations and the introduction of trace unknowns on the mesh skeleton
 
-// in 3D 
-// E,H ∈ (L^2(Ω))^3 
-// Ê ∈ H_0^1/2(Ω)(curl, Γ_h), Ĥ ∈ H^-1/2(curl, Γ_h)  
-//  i ω μ (H,F) + (E,∇ × F) + < Ê, F × n > = 0,      ∀ F ∈ H(curl,Ω)      
-// -i ω ϵ (E,G) + (H,∇ × G) + < Ĥ, G × n > = (J,G)   ∀ G ∈ H(curl,Ω)      
-//                                   Ê × n = E_0     on ∂Ω 
+// in 3D
+// E,H ∈ (L^2(Ω))^3
+// Ê ∈ H_0^1/2(Ω)(curl, Γ_h), Ĥ ∈ H^-1/2(curl, Γ_h)
+//  i ω μ (H,F) + (E,∇ × F) + < Ê, F × n > = 0,      ∀ F ∈ H(curl,Ω)
+// -i ω ϵ (E,G) + (H,∇ × G) + < Ĥ, G × n > = (J,G)   ∀ G ∈ H(curl,Ω)
+//                                   Ê × n = E_0     on ∂Ω
 // -------------------------------------------------------------------------
 // |   |       E      |      H      |      Ê       |       Ĥ      |  RHS    |
 // -------------------------------------------------------------------------
 // | F |  (E,∇ × F)   | i ω μ (H,F) | < n × Ê, F > |              |         |
 // |   |              |             |              |              |         |
-// | G |-(iωϵ+σ)(E,G) |  (H,∇ × G)  |              | < n × Ĥ, G > |  (J,G)  |  
+// | G |-(iωϵ+σ)(E,G) |  (H,∇ × G)  |              | < n × Ĥ, G > |  (J,G)  |
 // where (F,G) ∈  H(curl,Ω) × H(curl,Ω)
 
 // Here we use the "Adjoint Graph" norm on the test space i.e.,
@@ -89,27 +89,27 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order",
                   "Finite element order (polynomial degree)");
    args.AddOption(&rnum, "-rnum", "--number_of_wavelenths",
-                  "Number of wavelengths");    
+                  "Number of wavelengths");
    args.AddOption(&mu, "-mu", "--permeability",
                   "Permeability of free space (or 1/(spring constant)).");
    args.AddOption(&epsilon, "-eps", "--permittivity",
-                  "Permittivity of free space (or mass constant).");     
+                  "Permittivity of free space (or mass constant).");
    args.AddOption(&sigma, "-sigma", "--sigma",
-                  "conductivity");                                   
+                  "conductivity");
    args.AddOption(&delta_order, "-do", "--delta_order",
-                  "Order enrichment for DPG test space.");     
+                  "Order enrichment for DPG test space.");
    args.AddOption(&sr, "-sref", "--serial_ref",
-                  "Number of serial refinements.");                                              
+                  "Number of serial refinements.");
    args.AddOption(&pr, "-pref", "--parallel_ref",
-                  "Number of parallel refinements.");        
+                  "Number of parallel refinements.");
    args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
-                  "--no-static-condensation", "Enable static condensation.");                                            
+                  "--no-static-condensation", "Enable static condensation.");
    args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
                   "--no-visualization",
                   "Enable or disable GLVis visualization.");
    args.AddOption(&paraview, "-paraview", "--paraview", "-no-paraview",
-               "--no-paraview",
-               "Enable or disable ParaView visualization.");   
+                  "--no-paraview",
+                  "Enable or disable ParaView visualization.");
    args.Parse();
    if (!args.Good())
    {
@@ -142,25 +142,25 @@ int main(int argc, char *argv[])
    FiniteElementCollection *E_fec = new L2_FECollection(order-1,dim);
    ParFiniteElementSpace *E_fes = new ParFiniteElementSpace(&pmesh,E_fec,dim);
 
-   // Vector L2 space for H 
+   // Vector L2 space for H
    FiniteElementCollection *H_fec = new L2_FECollection(order-1,dim);
-   ParFiniteElementSpace *H_fes = new ParFiniteElementSpace(&pmesh,H_fec, dim); 
+   ParFiniteElementSpace *H_fes = new ParFiniteElementSpace(&pmesh,H_fec, dim);
 
-   // H^-1/2 (curl) space for Ê   
+   // H^-1/2 (curl) space for Ê
    FiniteElementCollection * hatE_fec = nullptr;
-   FiniteElementCollection * hatH_fec = nullptr; 
+   FiniteElementCollection * hatH_fec = nullptr;
    FiniteElementCollection * F_fec = nullptr;
    int test_order = order+delta_order;
    hatE_fec = new ND_Trace_FECollection(order,dim);
-   hatH_fec = new ND_Trace_FECollection(order,dim);   
+   hatH_fec = new ND_Trace_FECollection(order,dim);
    F_fec = new ND_FECollection(test_order, dim);
 
    ParFiniteElementSpace *hatE_fes = new ParFiniteElementSpace(&pmesh,hatE_fec);
    ParFiniteElementSpace *hatH_fes = new ParFiniteElementSpace(&pmesh,hatH_fec);
    FiniteElementCollection * G_fec = new ND_FECollection(test_order, dim);
 
-   Array<ParFiniteElementSpace * > trial_fes; 
-   Array<FiniteElementCollection * > test_fec; 
+   Array<ParFiniteElementSpace * > trial_fes;
+   Array<FiniteElementCollection * > test_fec;
    trial_fes.Append(E_fes);
    trial_fes.Append(H_fes);
    trial_fes.Append(hatE_fes);
@@ -196,13 +196,15 @@ int main(int argc, char *argv[])
    ParComplexDPGWeakForm * a = new ParComplexDPGWeakForm(trial_fes,test_fec);
 
    // (E,∇ × F)
-   a->AddTrialIntegrator(new TransposeIntegrator(new MixedCurlIntegrator(one)),nullptr,0,0);
+   a->AddTrialIntegrator(new TransposeIntegrator(new MixedCurlIntegrator(one)),
+                         nullptr,0,0);
    // -(i ω ϵ + σ) (E , G) = i (- ω ϵ E, G) - (σ E, G)
    a->AddTrialIntegrator(
       new TransposeIntegrator(new VectorFEMassIntegrator(negsigma_cf)),
       new TransposeIntegrator(new VectorFEMassIntegrator(negepsomeg)),0,1);
-   //  (H,∇ × G) 
-   a->AddTrialIntegrator(new TransposeIntegrator(new MixedCurlIntegrator(one)),nullptr,1,1);
+   //  (H,∇ × G)
+   a->AddTrialIntegrator(new TransposeIntegrator(new MixedCurlIntegrator(one)),
+                         nullptr,1,1);
    // < n×Ĥ ,G>
    a->AddTrialIntegrator(new TangentTraceIntegrator,nullptr,3,1);
    // test integrators
@@ -210,12 +212,13 @@ int main(int argc, char *argv[])
    a->AddTestIntegrator(new CurlCurlIntegrator(one),nullptr,1,1);
    // (G,δG)
    a->AddTestIntegrator(new VectorFEMassIntegrator(one),nullptr,1,1);
-   
+
    // i ω μ (H, F)
-   a->AddTrialIntegrator(nullptr,new TransposeIntegrator(new VectorFEMassIntegrator(muomeg)),1,0);
+   a->AddTrialIntegrator(nullptr,
+                         new TransposeIntegrator(new VectorFEMassIntegrator(muomeg)),1,0);
    // < n×Ê,F>
    a->AddTrialIntegrator(new TangentTraceIntegrator,nullptr,2,0);
-   // test integrators 
+   // test integrators
    // (∇×F,∇×δF)
    a->AddTestIntegrator(new CurlCurlIntegrator(one),nullptr,0,0);
    // (F,δF)
@@ -267,10 +270,10 @@ int main(int argc, char *argv[])
    }
 
    // internal bdr attributes
-   Array<int> internal_bdr({1, 3, 6, 9, 17, 157, 185, 75, 210, 211, 
-               212, 213, 214, 215, 216, 217, 218, 219, 
-               220, 221, 222, 223, 224, 225, 226, 227, 
-               228, 229, 230, 231, 232, 233, 234, 125});
+   Array<int> internal_bdr({1, 3, 6, 9, 17, 157, 185, 75, 210, 211,
+                            212, 213, 214, 215, 216, 217, 218, 219,
+                            220, 221, 222, 223, 224, 225, 226, 227,
+                            228, 229, 230, 231, 232, 233, 234, 125});
 
    for (int it = 0; it<=pr; it++)
    {
@@ -290,17 +293,17 @@ int main(int argc, char *argv[])
          ess_bdr.SetSize(pmesh.bdr_attributes.Max());
          one_bdr.SetSize(pmesh.bdr_attributes.Max());
          negone_bdr.SetSize(pmesh.bdr_attributes.Max());
-         ess_bdr = 1; 
-         // need to exclude these attributes 
-         for(int i = 0; i<internal_bdr.Size(); i++)
+         ess_bdr = 1;
+         // need to exclude these attributes
+         for (int i = 0; i<internal_bdr.Size(); i++)
          {
             ess_bdr[internal_bdr[i]-1] = 0;
          }
          hatE_fes->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
          one_bdr = 0;
          negone_bdr = 0;
-         one_bdr[234] = 1;  
-         negone_bdr[235] = 1;  
+         one_bdr[234] = 1;
+         negone_bdr[235] = 1;
       }
 
       if (myid == 0)
@@ -367,10 +370,10 @@ int main(int argc, char *argv[])
       tdof_offsets[0] = 0;
       int skip = (static_cond) ? 0 : 2;
       int k = (static_cond) ? 2 : 0;
-      for (int i=0; i<num_blocks;i++)
+      for (int i=0; i<num_blocks; i++)
       {
-         tdof_offsets[i+1] = trial_fes[i+k]->GetTrueVSize(); 
-         tdof_offsets[num_blocks+i+1] = trial_fes[i+k]->GetTrueVSize(); 
+         tdof_offsets[i+1] = trial_fes[i+k]->GetTrueVSize();
+         tdof_offsets[num_blocks+i+1] = trial_fes[i+k]->GetTrueVSize();
       }
       tdof_offsets.PartialSum();
 
@@ -391,10 +394,12 @@ int main(int argc, char *argv[])
 
       if (!static_cond)
       {
-         HypreBoomerAMG * solver_E = new HypreBoomerAMG((HypreParMatrix &)BlockA_r->GetBlock(0,0));
+         HypreBoomerAMG * solver_E = new HypreBoomerAMG((HypreParMatrix &)
+                                                        BlockA_r->GetBlock(0,0));
          solver_E->SetPrintLevel(0);
          solver_E->SetSystemsOptions(dim);
-         HypreBoomerAMG * solver_H = new HypreBoomerAMG((HypreParMatrix &)BlockA_r->GetBlock(1,1));
+         HypreBoomerAMG * solver_H = new HypreBoomerAMG((HypreParMatrix &)
+                                                        BlockA_r->GetBlock(1,1));
          solver_H->SetPrintLevel(0);
          solver_H->SetSystemsOptions(dim);
          M.SetDiagonalBlock(0,solver_E);
@@ -403,10 +408,12 @@ int main(int argc, char *argv[])
          M.SetDiagonalBlock(num_blocks+1,solver_H);
       }
 
-      
-      HypreAMS * solver_hatE = new HypreAMS((HypreParMatrix &)BlockA_r->GetBlock(skip,skip), hatE_fes);
-      HypreAMS * solver_hatH = new HypreAMS((HypreParMatrix &)BlockA_r->GetBlock(skip+1,skip+1), hatH_fes);
-      solver_hatE->SetPrintLevel(0);  
+
+      HypreAMS * solver_hatE = new HypreAMS((HypreParMatrix &)BlockA_r->GetBlock(skip,
+                                                                                 skip), hatE_fes);
+      HypreAMS * solver_hatH = new HypreAMS((HypreParMatrix &)BlockA_r->GetBlock(
+                                               skip+1,skip+1), hatH_fes);
+      solver_hatE->SetPrintLevel(0);
       solver_hatH->SetPrintLevel(0);
 
       M.SetDiagonalBlock(skip,solver_hatE);
@@ -424,7 +431,7 @@ int main(int argc, char *argv[])
       cg.SetRelTol(1e-5);
       cg.SetMaxIter(10000);
       cg.SetPrintLevel(1);
-      cg.SetPreconditioner(M); 
+      cg.SetPreconditioner(M);
       cg.SetOperator(blockA);
       cg.Mult(B, X);
 
@@ -454,10 +461,10 @@ int main(int argc, char *argv[])
          const char * keys = (it == 0 && dim == 2) ? "jRcml\n" : nullptr;
          char vishost[] = "localhost";
          int  visport   = 19916;
-         common::VisualizeField(E_out_r,vishost, visport, E.real(), 
-                               "Numerical Electric field (real part)", 0, 0, 500, 500, keys);
-         common::VisualizeField(H_out_r,vishost, visport, H.real(), 
-                               "Numerical Magnetic field (real part)", 501, 0, 500, 500, keys);   
+         common::VisualizeField(E_out_r,vishost, visport, E.real(),
+                                "Numerical Electric field (real part)", 0, 0, 500, 500, keys);
+         common::VisualizeField(H_out_r,vishost, visport, H.real(),
+                                "Numerical Magnetic field (real part)", 501, 0, 500, 500, keys);
       }
 
       if (paraview)
@@ -468,7 +475,9 @@ int main(int argc, char *argv[])
       }
 
       if (it == pr)
+      {
          break;
+      }
 
       pmesh.UniformRefinement();
 
@@ -497,5 +506,5 @@ int main(int argc, char *argv[])
    delete E_fes;
 
    return 0;
-}                                       
+}
 
